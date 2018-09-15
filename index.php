5760358871281cf2554a209464fcc4af9149abf5
Noah <noahcwoods@gmail.com>
2018-09-15 14:51:32 -0400

    ...

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 <link rel="stylesheet" href="style.css" />
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>login</title>
   </head>
   <body>
-        <div class="login">
-          <form action="" method="post">
-            <input type="text" name="username" placeholder="username"/> <br />
-            <input type="password" name="password" placeholder="password"/> <br />
-            <input type="submit" name="submit" />
-          </form>
-        </div>
-
+    <div class="container">
+      <div class="login">
+        <form action="" method="post">
+          <input type="text" name="username" placeholder="username"/><br />
+          <input type="password" name="password" placeholder="password"/><br />
+          <input type="submit" name="submit" />
+        </form>
+      </div>
+    </div>
   </body>
 </html>
