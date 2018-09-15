f042b7d31ba29363ef6c0c5b7554513ef6490568
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2018-09-15 13:28:51 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+        <div class="login">
+          <form>
+            <input type="text" name="username" placeholder="username"/>
+            <input type="password" name="password" placeholder="password"/>
+            <input type="submit" name="submit" />
+          </form>
+        </div>
+
+  </body>
+</html>
