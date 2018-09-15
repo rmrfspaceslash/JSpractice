295ef850cb37b954e989d9fe776f4867ee1017c7
Noah <noahcwoods@gmail.com>
2018-09-15 13:49:55 -0400

    ...

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   </head>
   <body>
         <div class="login">
-          <form>
+          <form action="" method="post">
             <input type="text" name="username" placeholder="username"/>
             <input type="password" name="password" placeholder="password"/>
             <input type="submit" name="submit" />
