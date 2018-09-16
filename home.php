4245829b40bf0796defb3bb808e97eb2561acdd7
Noah <noahcwoods@gmail.com>
2018-09-15 22:14:51 -0400

    ...

--- home.php
+++ home.php
@@ -14,18 +14,55 @@
     header('Location: index.php');
     session_destroy();
   }
- ?>
+
+  if (isset($_FILES['upload'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES['upload']['name']);
+    //echo $target_file;
+    $uploadver = true;
+  }
+
+  if (file_exists($target_file)) {
+    $uploadver = false;
+    echo "sorry, file already exists";
+    $ret = true;
+  }
+
+  if ($uploadver) {
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+  }
+
+  ?>
+  
+  <h5 style="color:red;">
+    <?php if ($ret) {echo $ret; } ?>
+  </h5>
 
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
-  <link rel"stylesheet" href="style.css" />
+<link rel="stylesheet" href="style.css" />
    <head>
      <meta charset="utf-8">
      <title>Homepage</title>
    </head>
    <body>
-     <form action="" method="post">
-       <input type="submit" name="logout" value="Logout"/>
-     </form>
+     <div class="container">
+       <div class="login">
+         <form action="" method="post">
+           <input type="file" name="upload"> <br />
+           <input type="submit" name="store" value="upload">
+           <input type="submit" name="logout" value="Logout"/>
+         </form>
+       </div>
+    </div>
    </body>
  </html>
+
+
+
+ Upload your file.
+ <form action="" method="post" enctype="multipart/form-data">
+   <input type="file" name="upload">
+   <br />
+   <input type="submit" name="Submit">
+ </form>
