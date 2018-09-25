11f4393249c02ed5607b9612bcfa19079748a0e0
Noah <noahcwoods@gmail.com>
2018-09-25 10:50:25 -0400

    ...

--- edituser.php
+++ edituser.php
@@ -10,6 +10,40 @@ if (!isset($_SESSION['username'])) {
   header("Location: index.php");
 }
 
+if ($_POST['userid'] != null) {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //assign user inputs to variables
+    $username = $_POST['userid'];
+      //add entry into database
+      $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
+      $conn->query($sql);
+  }
+
+}
+if ($_POST['username'] != null) {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //assign user inputs to variables
+    $username = $_POST['userid'];
+      //add entry into database
+      $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
+      $conn->query($sql);
+  }
+
+}
+if ($_POST['password'] != null) {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //assign user inputs to variables
+    $username = $_POST['userid'];
+      //add entry into database
+      $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
+      $conn->query($sql);
+  }
+
+}
+
 if (isset($_GET['id']) && $_GET['edit'] == "edit") {
   require('dbconnect.php');
   $sql="SELECT * FROM users WHERE userid=".$_GET['id']; //int need not be quoted because they are ints
@@ -20,8 +54,8 @@ if (isset($_GET['id']) && $_GET['edit'] == "edit") {
   while ($row = $result->fetch_assoc()) {
 ?>
   <input name="userid" type="text" disabled value="<?php echo $row['userid'];?>"> <br>
-  <input name="username" type="text" disabled value="<?php echo $row['username'];?>"> <br>
-  <input name="password" type="text" disabled value="<?php echo $row['password'];?>"> <br>
+  <input name="username" type="text" value="<?php echo $row['username'];?>"> <br>
+  <input name="password" type="text" value="<?php echo $row['password'];?>"> <br>
   <input name="submit" type="submit" value="change">
 <?php
   }
