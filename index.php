8a77feaa9a362ad6a23feeb4fb43abf2d5c5224a
noah <noahcwoods@gmail.com>
2018-10-10 11:43:49 -0400

    ...

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 
   //require database connection before continue
   require('dbconnect.php');
-  require('index.js');
-
-
-
+  
   //start session if need to
   if (!isset($_SESSION)) {
     session_start();
