1891c35f32fbec8a6675b33024c31f3718475e43
noah <noahcwoods@gmail.com>
2018-10-11 10:49:47 -0400

    ...

--- shell.php
+++ shell.php
@@ -29,7 +29,7 @@ foreach ($testarray as $key => $value) {
 }
 
 $users = shell_exec('who');
-$users = preg_split("/[\s, ]+/", $users);
+$users = preg_split("/[0-9, \s, \W]+/", $users);
 echo $users;
 foreach ($users as $key => $value) {
   echo $value;
