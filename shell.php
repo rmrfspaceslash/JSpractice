036bead10ffba62022254c44bc9fdc8ec213707f
noah <noahcwoods@gmail.com>
2018-10-11 10:11:48 -0400

    ...

--- shell.php
+++ shell.php
@@ -5,7 +5,18 @@
   $pwd = shell_exec('pwd');
   echo " <pre>$pwd</pre>";
 
-  
-
+  $check = file_exists("test");
+if ($check) {
+  $checkDir = is_dir("test");
+  if ($checkDir) {
+    echo "This exists and is a directory!";
+  }
+  else {
+    echo "This exists, but it's not a directory.";
+  }
+}
+else {
+  mkdir("test");
+}
 
  ?>
